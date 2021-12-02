361f4b0c521191cd3ce72e13844836e513c6cb47
Sam Horn <samuel.horn1@gmail.com>
2021-12-02 20:54:29 +1000

    Day 2: part two first attempt

--- day02-dive.php
+++ day02-dive.php
@@ -31,3 +31,35 @@ foreach ($commands as $command) {
 $multiplied_position = $horizontal_position * $depth;
 
 echo $multiplied_position . "\n";
+
+/**
+ *
+ * Part Two: get your final horizontal position multiplied by your final depth, taking aim into account
+ *
+ */
+
+$horizontal_position = 0;
+$depth = 0;
+$aim = 0;
+
+foreach ($commands as $command) {
+    [$direction, $distance] = explode(' ', $command);
+    $distance = (int) $distance;
+
+    switch ($direction) {
+        case 'forward':
+            $horizontal_position += $distance;
+            $depth += ($aim * $distance);
+            break;
+        case 'up':
+            $aim -= $distance;
+            break;
+        case 'down':
+            $aim += $distance;
+            break;
+    }
+}
+
+$multiplied_position = $horizontal_position * $depth;
+
+echo $multiplied_position . "\n";
