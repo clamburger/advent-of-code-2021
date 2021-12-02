267ac704c1d4e681967f7557c30482b6fe5f713e
Sam Horn <samuel.horn1@gmail.com>
2021-12-02 20:27:31 +1000

    Day 1: first attempt

---
+++ day01-sonar-sweep.php
@@ -0,0 +1,23 @@
+<?php
+$depths = file(__DIR__ . '/day01.input.txt');
+$depths = array_map('trim', $depths);
+
+/**
+ *
+ * Part One: count the number of times the depth increases
+ *
+ */
+
+$previous_depth = null;
+
+$deeper_depths = 0;
+
+foreach ($depths as $depth) {
+    if ($previous_depth === null || $depth > $previous_depth) {
+        $deeper_depths++;
+    }
+
+    $previous_depth = $depth;
+}
+
+echo $deeper_depths . "\n";
