4e2a987531d12f9936a2bf0b7eea4638b3c42263
Sam Horn <samuel.horn1@gmail.com>
2021-12-05 18:32:01 +1000

    Day 5: part two first attempt

--- src/Puzzles/Day05HydrothermalVenture.php
+++ src/Puzzles/Day05HydrothermalVenture.php
@@ -44,6 +44,45 @@ class Day05HydrothermalVenture extends AbstractPuzzle
 
     public function getPartTwoAnswer(): int
     {
-        return 0;
+        $danger_zones = [];
+
+        foreach ($this->input->lines as $vent) {
+            $raw_coords = explode(' -> ', $vent);
+
+            list($x1, $y1) = explode(',', $raw_coords[0]);
+            list($x2, $y2) = explode(',', $raw_coords[1]);
+
+            // Diagonal
+            if ($x1 !== $x2 && $y1 !== $y2) {
+                for ($x = $x1, $y = $y1; $x != (int)$x2 + ($x2 <=> $x1); $x += $x2 <=> $x1, $y += $y2 <=> $y1) {
+                    if (!isset($danger_zones["$x,$y"])) {
+                        $danger_zones["$x,$y"] = 0;
+                    }
+                    $danger_zones["$x,$y"]++;
+                }
+            }
+
+            // Horizontal
+            if ($y1 === $y2) {
+                for ($x = $x1; $x != (int)$x2 + ($x2 <=> $x1); $x += $x2 <=> $x1) {
+                    if (!isset($danger_zones["$x,$y1"])) {
+                        $danger_zones["$x,$y1"] = 0;
+                    }
+                    $danger_zones["$x,$y1"]++;
+                }
+            }
+
+            // Vertical
+            if ($x1 === $x2) {
+                for ($y = $y1; $y != (int)$y2 + ($y2 <=> $y1); $y += $y2 <=> $y1) {
+                    if (!isset($danger_zones["$x1,$y"])) {
+                        $danger_zones["$x1,$y"] = 0;
+                    }
+                    $danger_zones["$x1,$y"]++;
+                }
+            }
+        }
+
+        return count(array_filter($danger_zones, fn ($number) => $number >= 2));
     }
 }
