01b768d8153989225301f9c2a956bc796f27b5ab
Sam Horn <samuel.horn1@gmail.com>
2021-12-07 19:48:36 +1000

    Day 7: part one first attempt

---
+++ src/Puzzles/Day07TheTreacheryOfWhales.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Puzzles;
+
+class Day07TheTreacheryOfWhales extends AbstractPuzzle
+{
+    protected static int $day_number = 7;
+
+    private array $crabs;
+
+    private function resetCrabs(): void
+    {
+        $this->crabs = explode(',', $this->input->lines[0]);
+    }
+
+    public function getPartOneAnswer(): int
+    {
+        $this->resetCrabs();
+
+        $limit = max($this->crabs);
+
+        $min = null;
+
+        for ($position = 0; $position <= $limit; $position++) {
+            $fuel = 0;
+            foreach ($this->crabs as $crab) {
+                $fuel += abs($crab - $position);
+            }
+
+            if ($fuel < $min || $min === null) {
+                $min = $fuel;
+            }
+        }
+
+        return $min;
+    }
+
+    public function getPartTwoAnswer(): int
+    {
+        return 0;
+    }
+}
