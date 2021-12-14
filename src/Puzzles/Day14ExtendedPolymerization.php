b15c1f9ea1422143192a0ca14ab01b3d08a10acf
Sam Horn <samuel.horn1@gmail.com>
2021-12-14 19:33:27 +1000

    Day 14: part two

--- src/Puzzles/Day14ExtendedPolymerization.php
+++ src/Puzzles/Day14ExtendedPolymerization.php
@@ -6,16 +6,26 @@ class Day14ExtendedPolymerization extends AbstractPuzzle
 {
     protected static int $day_number = 14;
 
+    private string $template;
+    private array $rules;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->template = $this->input->raw_blocks[0];
+        $this->rules = array_map(fn ($rule) => explode(' -> ', $rule), explode("\n", $this->input->raw_blocks[1]));
+    }
+
     public function getPartOneAnswer(): int
     {
-        $template = $this->input->raw_blocks[0];
-        $rules = array_map(fn ($rule) => explode(' -> ', $rule), explode("\n", $this->input->raw_blocks[1]));
+        $template = $this->template;
 
         for ($i = 1; $i <= 10; $i++) {
-            foreach ($rules as $rule) {
+            foreach ($this->rules as $rule) {
                 $replace = $rule[0][0] . strtolower($rule[1]) . $rule[0][1];
-                $template = preg_replace("/{$rule[0]}/", $replace, $template);
-                $template = preg_replace("/{$rule[0]}/", $replace, $template);
+                $template = str_replace($rule[0], $replace, $template);
+                $template = str_replace($rule[0], $replace, $template);
             }
             $template = strtoupper($template);
         }
@@ -29,7 +39,50 @@ class Day14ExtendedPolymerization extends AbstractPuzzle
 
     public function getPartTwoAnswer(): int
     {
-        return 0;
+        $pairs = [];
+        $rules = [];
+
+        foreach ($this->rules as $rule) {
+            // Store the two new pairs that are added when a rule is applied
+            $rules[$rule[0]] = [
+                $rule[0][0] . $rule[1],
+                $rule[1] . $rule[0][1]
+            ];
+        }
+
+        for ($i = 0; $i < strlen($this->template) - 1; $i++) {
+            $pair = substr($this->template, $i, 2);
+            $pairs[$pair] = ($pairs[$pair] ?? 0) + 1;
+        }
+
+        for ($i = 1; $i <= 40; $i++) {
+            $pairs_copy = $pairs;
+            foreach ($pairs_copy as $pair => $count) {
+                if (!isset($rules[$pair])) {
+                    continue;
+                }
+
+                $new_pairs = $rules[$pair];
+
+                $pairs[$pair] -= $count;
+                $pairs[$new_pairs[0]] = ($pairs[$new_pairs[0]] ?? 0) + $count;
+                $pairs[$new_pairs[1]] = ($pairs[$new_pairs[1]] ?? 0) + $count;
+            }
+        }
+
+        $elements = [];
+
+        foreach ($pairs as $pair => $count) {
+            $element = $pair[0];
+            $elements[$element] = ($elements[$element] ?? 0) + $count;
+        }
+
+        $last_char = $this->template[strlen($this->template)-1];
+        $elements[$last_char]++;
+
+        asort($elements);
+
+        return array_pop($elements) - array_shift($elements);
     }
 }
 
